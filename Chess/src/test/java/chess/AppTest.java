74a5ceb8b96dbfa8024c872c4246faf27b494060
TheGreatRy <158355076+TheGreatRy@users.noreply.github.com>
2026-03-11 11:26:00 -0600

    Added Maven to Run Tests (I could not get it to work on my own, everyone say "Thank you Claude")

---
+++ Chess/src/test/java/chess/AppTest.java
@@ -0,0 +1,14 @@
+package chess;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import org.junit.jupiter.api.Test;
+
+public class AppTest {
+    
+    @Test
+    public void test()
+    {
+        assertEquals(2, 1+1);
+    }
+}
